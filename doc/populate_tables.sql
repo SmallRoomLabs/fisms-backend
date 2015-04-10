7ebdffcd2eeba429b16a97e31e4a8a98db974994
Mats Engstrom <mats.engstrom@gmail.com>
2015-04-10 05:50:20 +0000

    Script to populating owners and systems

---
+++ doc/populate_tables.sql
@@ -0,0 +1,12 @@
+INSERT INTO owner (email,pw) VALUES ('mats.engstrom@gmail.com','z');
+INSERT INTO owner (email,pw) VALUES ('raymond@viacard.com','z');
+INSERT INTO owner (email,pw) VALUES ('mats@viacard.com','z');
+
+INSERT INTO system (owner_id,country,phone,token) VALUES (1,'.my','+60132949422','token-1-1');
+INSERT INTO system (owner_id,country,phone,token) VALUES (1,'.my','+60130000002','token-1-2');
+INSERT INTO system (owner_id,country,phone,token) VALUES (2,'.ph','+63100000001','token-2-1');
+INSERT INTO system (owner_id,country,phone,token) VALUES (3,'.th','+66810000001','token-3-1');
+INSERT INTO system (owner_id,country,phone,token) VALUES (3,'.th','+66810000002','token-3-2');
+INSERT INTO system (owner_id,country,phone,token) VALUES (3,'.th','+66810000003','token-3-3');
+INSERT INTO system (owner_id,country,phone,token) VALUES (3,'.th','+66810000004','token-3-4');
+
